e815f855ec1fcd6fbade7685abbdcfd074716b8a
Jessica Antunes <jessantunes@protonmail.com>
2021-04-13 20:05:21 -0400

    add password_digest to shelters table migration file

--- db/migrate/20210413235606_create_shelters_table.rb
+++ db/migrate/20210413235606_create_shelters_table.rb
@@ -2,6 +2,7 @@ class CreateSheltersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :shelters do |t|
       t.string :email
+      t.string :password_digest
       t.string :shelter_name
       t.string :hours
       t.string :phone
