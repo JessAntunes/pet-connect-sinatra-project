f2df93879f721df19c3183a958ed0a1648bf8c7a
Jessica Antunes <jessantunes@protonmail.com>
2021-04-13 22:30:01 -0400

    create animal controllers

--- app/controllers/animal_controller.rb
+++ app/controllers/animal_controller.rb
@@ -1,7 +1,40 @@
 class AnimalController < ApplicationController
 
-
-
+    get '/animals' do
+        @animals = Animal.all
+        erb :index
+    end 
+    
+    get '/animals/new' do 
+        erb :new
+    end
+    
+    post '/animals' do 
+        @animal = Animal.create(params)
+        redirect "/animals/#{@animal.id}"
+    end
+    
+    get '/animals/:id' do 
+        @animal = Animal.find_by_id(params[:id])
+        erb :show
+    end 
+    
+    get '/animals/:id/edit' do 
+        @animal = Animal.find_by_id(params[:id])
+        erb :edit
+    end 
+    
+    patch '/animals/:id' do 
+        @animal = Animal.find_by_id(params[:id])
+        @animal.update(params)
+        redirect to "/animals/#{@animal.id}"
+    end
+    
+    delete '/animals/:id' do 
+        @animal = Animal.find_by_id(params[:id])
+        @animal.delete
+        redirect to '/animals'
+    end
 
 
 end
