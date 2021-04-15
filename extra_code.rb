135cf6a3897782a082092a6c3bb722bef2fcbe10
Jessica Antunes <jessantunes@protonmail.com>
2021-04-14 20:46:21 -0400

    add nav bar, new logo, and button

---
+++ extra_code.rb
@@ -0,0 +1,7 @@
+# <header><img src="/images/Pet-Connect-Full-Branding.png" class="img-responsive main-image">  
+#        <%if logged_in?%>
+#       <a href="/logout" >Logout</a> 
+#        <%else%>
+#        <a href="/login" >Login</a> 
+#        <%end%>
+#       </header>
